0b758e0671ad06846e7f1699928e909070e33a76
Paul Martins <pmartins@controlsoft.ie>
2021-07-28 10:44:44 +0100

    - Added enumerable tests

--- src/MooVC.Infrastructure.Serialization.MessagePack.Tests/TypedSerializerTests/WhenInstancesAreSerialized.cs
+++ src/MooVC.Infrastructure.Serialization.MessagePack.Tests/TypedSerializerTests/WhenInstancesAreSerialized.cs
@@ -2,7 +2,9 @@
 {
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using System.Threading.Tasks;
+    using MooVC.Collections.Generic;
     using Xunit;
 
     public sealed class WhenInstancesAreSerialized
@@ -101,7 +103,7 @@
                 default,
                 "Something something dark side...");
 
-            var serializer = new TypelessSerializer();
+            var serializer = new TypedSerializer();
 
             IEnumerable<byte> stream = await serializer.SerializeAsync(original);
             SerializableRecord deserialized = await serializer.DeserializeAsync<SerializableRecord>(stream);
@@ -133,6 +135,205 @@
             ISerializableInstance deserialized = await serializer.DeserializeAsync<ISerializableInstance>(stream);
 
             AssertEqual(original, deserialized);
+        }
+
+        [Fact]
+        public async Task GivenAnInstancesOfAClassThenACloneOfThatInstanceIsDeserializedAsync()
+        {
+            IEnumerable<SerializableClass> originals = new[]
+            {
+                new SerializableClass
+                {
+                    Array = new ulong[] { 1, 2, 3 },
+                    Integer = 25,
+                    String = "Something something",
+                },
+                new SerializableClass
+                {
+                    Array = new ulong[] { 4, 5, 6 },
+                    Integer = 3,
+                    String = "dark side...",
+                },
+            };
+
+            var serializer = new TypedSerializer();
+
+            IEnumerable<byte> stream = await serializer.SerializeAsync(originals);
+
+            IEnumerable<SerializableClass> deserialized = await serializer
+                .DeserializeAsync<IEnumerable<SerializableClass>>(stream);
+
+            AssertEqual(originals, deserialized);
+        }
+
+        [Fact]
+        public async Task GivenAnInstancesOfAClassWhenSerializedToAStreamThenACloneOfThatInstanceIsDeserializedAsync()
+        {
+            IEnumerable<SerializableClass> originals = new[]
+            {
+                new SerializableClass
+                {
+                    Array = new ulong[] { 1, 2, 3 },
+                    Integer = 25,
+                    String = "Something something",
+                },
+                new SerializableClass
+                {
+                    Array = new ulong[] { 4, 5, 6 },
+                    Integer = 9,
+                    String = "dark side...",
+                },
+            };
+
+            using var stream = new MemoryStream();
+            var serializer = new TypedSerializer();
+
+            await serializer.SerializeAsync(originals, stream);
+
+            stream.Position = 0;
+
+            IEnumerable<SerializableClass> deserialized = await serializer
+                .DeserializeAsync<IEnumerable<SerializableClass>>(stream);
+
+            AssertEqual(originals, deserialized);
+        }
+
+        [Fact]
+        public async Task GivenAnInstancesOfAClassWithAReferencedObjectWhenSerializedToAStreamThenACloneOfThatInstanceIsDeserializedAsync()
+        {
+            IEnumerable<ISerializableInstance> originals = new[]
+            {
+                new SerializableClass
+                {
+                    Array = new ulong[] { 1, 2, 3 },
+                    Integer = 25,
+                    Object = new SerializableClass(),
+                    String = "Something something dark side...",
+                },
+            };
+
+            using var stream = new MemoryStream();
+
+            var serializer = new TypedSerializer();
+
+            await serializer.SerializeAsync(originals, stream);
+
+            stream.Position = 0;
+
+            IEnumerable<ISerializableInstance> deserialized = await serializer
+                .DeserializeAsync<IEnumerable<ISerializableInstance>>(stream);
+
+            AssertEqual(originals, deserialized);
+        }
+
+        [Fact]
+        public async Task GivenAnInstancseOfARecordThenACloneOfThatInstanceIsDeserializedAsync()
+        {
+            IEnumerable<SerializableRecord> originals = new[]
+            {
+                new SerializableRecord(
+                    new ulong[] { 1, 2, 3 },
+                    25,
+                    default,
+                    "Something something"),
+                new SerializableRecord(
+                    new ulong[] { 4, 5, 6 },
+                    7,
+                    default,
+                    "dark side..."),
+            };
+
+            var serializer = new TypedSerializer();
+
+            IEnumerable<byte> stream = await serializer.SerializeAsync(originals);
+
+            IEnumerable<SerializableRecord> deserialized = await serializer
+                .DeserializeAsync<IEnumerable<SerializableRecord>>(stream);
+
+            AssertEqual(originals, deserialized);
+        }
+
+        [Fact]
+        public async Task GivenAnInstancesOfARecordWhenSerializedToAStreamThenACloneOfThatInstanceIsDeserializedAsync()
+        {
+            IEnumerable<SerializableRecord> originals = new[]
+            {
+                new SerializableRecord(
+                    new ulong[] { 1, 2, 3 },
+                    25,
+                    default,
+                    "dark side..."),
+                new SerializableRecord(
+                    new ulong[] { 4, 5, 6 },
+                    3,
+                    default,
+                    "dark side..."),
+            };
+
+            using var stream = new MemoryStream();
+            var serializer = new TypedSerializer();
+
+            await serializer.SerializeAsync(originals, stream);
+
+            stream.Position = 0;
+
+            IEnumerable<SerializableRecord> deserialized = await serializer
+                .DeserializeAsync<IEnumerable<SerializableRecord>>(stream);
+
+            AssertEqual(originals, deserialized);
+        }
+
+        [Fact]
+        public async Task GivenAnInstancesOfARecordWithAReferencedObjectWhenSerializedToAStreamThenACloneOfThatInstanceIsDeserializedAsync()
+        {
+            IEnumerable<ISerializableInstance> originals = new[]
+            {
+                new SerializableRecord(
+                    new ulong[] { 1, 2, 3 },
+                    25,
+                    new SerializableRecord(
+                        new ulong[] { 1, 5 },
+                        3,
+                        default,
+                        "Something else..."),
+                    "Something something"),
+                new SerializableRecord(
+                    new ulong[] { 4, 5, 6 },
+                    5,
+                    new SerializableRecord(
+                        new ulong[] { 2, 4 },
+                        2,
+                        default,
+                        "Something else..."),
+                    "dark side..."),
+            };
+
+            using var stream = new MemoryStream();
+
+            var serializer = new TypedSerializer();
+
+            await serializer.SerializeAsync(originals, stream);
+
+            stream.Position = 0;
+
+            IEnumerable<ISerializableInstance> deserialized = await serializer
+                .DeserializeAsync<IEnumerable<ISerializableInstance>>(stream);
+
+            AssertEqual(originals, deserialized);
+        }
+
+        private static void AssertEqual(
+            IEnumerable<ISerializableInstance> originals,
+            IEnumerable<ISerializableInstance> deserialized)
+        {
+            Assert.Equal(originals.Count(), deserialized.Count());
+
+            originals.For((index, original) =>
+            {
+                ISerializableInstance? pair = deserialized.ElementAt(index);
+
+                AssertEqual(original, pair);
+            });
         }
 
         private static void AssertEqual(ISerializableInstance? original, ISerializableInstance? deserialized)
